f303d78578586ee29421a5c9cf33959d3bc7974a
Alexandr <tribisalexandr@yandex.ru>
2022-07-31 17:13:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = expr;
+    let arr = [];
+    let strOut = [];
+    // console.log(str.replace(/^.{10}/, ''));
+
+    for (let i = 0; i < expr.length / 10; i++) {
+        arr.push(str.slice(0, 10));
+        str = str.replace(/^.{10}/, '');
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        let arr1 = arr[i];
+        if (arr[i] === '**********') {
+            strOut.push(' ');
+        } else {
+            for (let j = 0; j < arr[i].length / 2; j++) {
+                if (arr1.slice(0, 2) === '10') {
+                    strOut.push('.');
+                } else if (arr1.slice(0, 2) === '11') {
+                    strOut.push('-');
+                } else {
+                    strOut.push('');
+                }
+                arr1 = arr1.replace(/^.{2}/, '');
+            }
+        }
+        strOut.push(' ');
+    }
+
+    let str1 = strOut.join('').toString();
+    console.log(str1);
+    console.log(arr);
+
+    return str1
+    .split('   ')
+    .map(
+      a => a
+      .split(' ')
+      .map(
+        b => MORSE_TABLE[b]
+      ).join('')
+    ).join(' ');
 }
 
 module.exports = {
